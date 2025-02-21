bc4cfba1ee6859cb7fc9646b8a73de8e111c5083
Jordan Broomfield <Jordan.Broomfield2004@yahoo.com>
2025-02-21 14:47:26 -0500

    Implemented Enemies in Game and Bullets Traveling Farther

--- Space Invaders Game/MainWindow.xaml.cs
+++ Space Invaders Game/MainWindow.xaml.cs
@@ -51,9 +51,10 @@ namespace Space_Invaders_Game
 
             myCanvas.Focus();
 
+            makeEnemies(10);
 
         }
-        //Up Down Left Right Movement of Player
+        // Game Loop for the Game to Start and Run Functions
         private void GameLoop(object sender, EventArgs e)
         {
             if( goLeft == true && Canvas.GetLeft(player)>0)
@@ -64,6 +65,32 @@ namespace Space_Invaders_Game
             if(goRight == true && Canvas.GetLeft(player) + 80 < Application.Current.MainWindow.Width)
             {
                 Canvas.SetLeft(player, Canvas.GetLeft(player) + 10);
+            }
+
+            bulletTimer -= 3;
+
+            if (bulletTimer < 0)
+            {
+                enemyBulletMaker(Canvas.GetLeft(player) + 20, 10);
+
+                bulletTimer = bulletTimerLimit;
+
+            }
+
+            foreach (var x in myCanvas.Children.OfType<Rectangle>())
+            {
+                if(x is Rectangle && (string)x.Tag == "bullet")
+                {
+                    Canvas.SetTop(x, Canvas.GetTop(x) - 20);
+                }
+
+                if(Canvas.GetTop(x) < 10)
+                {
+                    itemsToRemove.Add(x);
+                }
+
+                Rect bullet = new Rect (Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
             }
         }
         //Key is Down
@@ -112,14 +139,86 @@ namespace Space_Invaders_Game
             }
 
         }
-
+        //Enemy Bullets
         private void enemyBulletMaker(double x, double  y)
         {
 
-        }
+            Rectangle enemmyBullet = new Rectangle
+            {
+                Tag = "enemyBullet",
+                Height = 40,
+                Width = 15,
+                Fill = Brushes.Yellow,
+                Stroke = Brushes.Black,
+                StrokeThickness = 5
+            };
 
+            Canvas.SetTop(enemmyBullet, y);
+            Canvas.SetLeft(enemmyBullet, x);
+
+            myCanvas.Children.Add(enemmyBullet);
+
+        }
+        // Making Enemies for Player to Shoot
         private void makeEnemies( int limit)
         {
+
+            int left = 800;
+
+            for (int i = 0; i < limit; i++)
+            {
+                ImageBrush enemySkin = new ImageBrush();
+
+                Rectangle newEnemy = new Rectangle
+                {
+                    Tag = "enemy",
+                    Height = 45,
+                    Width = 45,
+                    Fill = enemySkin
+                };
+
+                Canvas.SetTop(newEnemy, 10);
+                Canvas.SetLeft(newEnemy, left);
+                myCanvas.Children.Add(newEnemy);
+                left -= 60;
+
+                enemyImages++;
+
+                if (enemyImages > 8)
+                {
+                    enemyImages = 1;
+                }
+
+                switch (enemyImages)
+                {
+                    case 1:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/invader1.gif"));
+                        break;
+                    case 2:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/invader2.gif"));
+                        break;
+                    case 3:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/invader3.gif"));
+                        break;
+                    case 4:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/invader4.gif"));
+                        break;
+                    case 5:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/invader5.gif"));
+                        break;
+                    case 6:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/invader6.gif"));
+                        break;
+                    case 7:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/invader7.gif"));
+                        break;
+                    case 8:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/invader8.gif"));
+                        break;
+                }
+
+
+            }
 
         }
         private void showGameOver(string msg )
